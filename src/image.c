bd395d36facabf3d7a746d8f9b8b94adde0616a3
lgstuehrmann <lgstuehrmann18@gmail.com>
2018-03-01 13:04:29 -0500

    added functionality for printing bounding box coords to a text file

--- src/image.c
+++ src/image.c
@@ -230,7 +230,10 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
             if(right > im.w-1) right = im.w-1;
             if(top < 0) top = 0;
             if(bot > im.h-1) bot = im.h-1;
-
+			FILE *fp;
+			fp = fopen("results.txt", "w");
+			fprintf(fp, "Bounding Box: Left=%d, Top=%d, Right=%d, Bottom=%d\n", left, top, right, bot);
+			printf("Bounding Box: Left=%d, Top=%d, Right=%d, Bottom=%d\n", left, top, right, bot);
             draw_box_width(im, left, top, right, bot, width, red, green, blue);
             if (alphabet) {
                 image label = get_label(alphabet, names[class], (im.h*.03)/10);
